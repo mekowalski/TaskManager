696b371b4b970e3604d9ca96a61dd9db499e1a9f
Malind Kowalski <malind@agsnm.com>
2025-03-17 10:56:14 -0600

    added image ad string resources, completed functions and called in main and preview

--- app/src/main/java/com/example/taskmanager/MainActivity.kt
+++ app/src/main/java/com/example/taskmanager/MainActivity.kt
@@ -3,7 +3,7 @@ package com.example.taskmanager
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxHeight
@@ -11,19 +11,22 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.taskmanager.ui.theme.TaskManagerTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContent {
             TaskManagerTheme {
                 Surface(
@@ -31,7 +34,7 @@ class MainActivity : ComponentActivity() {
                     color = MaterialTheme.colorScheme.background
                 )
                 {
-                    //CompletedTask
+                    CompletedTask()
                 }
             }
         }
@@ -44,13 +47,25 @@ fun CompletedTask() {
     Column(
         //center align content vertically/horizontally on screen
         modifier = Modifier
-            .fillMaxWidth() .fillMaxHeight(),
+            .fillMaxWidth()
+            .fillMaxHeight(),
         verticalArrangement = Arrangement.Center,
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
         //image from download
+        val image = painterResource(R.drawable.ic_task_completed)
+        Image(painter = image, contentDescription = null)
         //text1: bold, top padding 24dp, bottom padding 9dp (All tasks completed)
+        Text(
+            text = stringResource(R.string.all_tasks_completed),
+            modifier = Modifier.padding(top = 24.dp, bottom = 9.dp),
+            fontWeight = FontWeight.Bold
+        )
         //text2: font size 15sp (Nice work!)
+        Text(
+            text = stringResource(R.string.nice_work),
+            fontSize = 15.sp
+        )
     }
 }
 
@@ -59,6 +74,6 @@ fun CompletedTask() {
 @Composable
 fun CompletedTaskPreview() {
     TaskManagerTheme {
-        //CompletedTask() called
+        CompletedTask()
     }
 }
